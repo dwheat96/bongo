9b7ea3de660e84dd743a393ca7bc753a5285ae3b
Derek Wheat <trigostruck@gmail.com>
2021-08-21 01:37:21 -0600

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCK4PhNHio8p6qXsMY2nF9ef9ulaqcebiQ",
+    authDomain: "bongo-4188a.firebaseapp.com",
+    projectId: "bongo-4188a",
+    storageBucket: "bongo-4188a.appspot.com",
+    messagingSenderId: "474517155335",
+    appId: "1:474517155335:web:1f097f3c2cdaa9e1b1c4a2",
+    measurementId: "G-X9JGVN9554"
+  };
