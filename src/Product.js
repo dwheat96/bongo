c55ecc2b45831ab171b9bb633466b253ca23792b
Derek Wheat <trigostruck@gmail.com>
2021-08-21 03:13:48 -0600

    building product tiles

---
+++ src/Product.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Product.css'
+
+function Product() {
+    return (
+        <div className="product">
+            <div className="product_info">
+                <p>Product Name</p>
+                <p className="product_price">
+                    <small>product price</small>
+                    </p>
+            </div>
+        </div>
+    )
+}
+
+export default Product
