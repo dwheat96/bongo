48281b8dadb2de0a63fe1730453062d0517072a8
Derek Wheat <trigostruck@gmail.com>
2021-08-21 16:06:32 -0600

    starting to add redux/react context

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Subtotal.css'
+import React from 'react';
+import './Subtotal.css';
+import CurrencyFormat from "react-currency-format";
 
 function Subtotal() {
     return (
         <div className="subtotal">
-            
+            <CurrencyFormat
+                renderText={(value) => (
+                <>
+                    <p>
+                    Subtotal (0 items): <strong>{value}</strong>
+                    </p>
+                    <small className="subtotal_gift">
+                    <input type="checkbox" /> This order contains a gift
+                    </small>
+                </>
+                )}
+                decimalScale={2}
+                value={0}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+            <button>Proceed to Checkout</button>
         </div>
     )
 }
