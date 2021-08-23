d39876ce9a392a9bee611a28d55aa8ee86c175ec
Derek Wheat <trigostruck@gmail.com>
2021-08-23 17:52:00 -0600

    get er deployed https://bongo-4188a.web.app/Orders

--- src/Order.js
+++ src/Order.js
@@ -10,7 +10,7 @@ function Order({ order }) {
         <div className='order'>
             <h2>Order</h2>
             <p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>
-            <p className="order__id">
+            <p className="order_id">
                 <small>{order.id}</small>
             </p>
             {order.data.basket?.map(item => (
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/bongo-4188a/us-central1/api'
+    baseURL: 'https://us-central1-bongo-4188a.cloudfunctions.net/api'
+    // 'http://localhost:5001/bongo-4188a/us-central1/api'
 
 })
 
